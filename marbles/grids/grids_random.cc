99156a5b7374d696e6545d0cdc2b4a704d7cd228
Leandro Bolivar <leandrob131@gmail.com>
2026-05-10 09:59:59 -0500

    First try

---
+++ marbles/grids/grids_random.cc
@@ -0,0 +1,26 @@
+// Copyright 2009 Emilie Gillet.
+//
+// Author: Emilie Gillet (emilie.o.gillet@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+//
+// Fast 16-bit pseudo random number generator.
+
+#include "marbles/grids/grids_random.h"
+
+namespace marbles {
+
+uint16_t GridsRandom::rng_state_ = 0x1234;
+
+}  // namespace marbles
